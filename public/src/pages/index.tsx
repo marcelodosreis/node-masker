c16b896b27c01c6f6b8e2c2067db5859f13c0ac5
marcelo.reis1 <marcelo.reis@sympla.com.br>
2021-05-18 17:00:01 -0300

    feat: new docs file

--- public/src/pages/index.tsx
+++ public/src/pages/index.tsx
@@ -1,11 +1,17 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { PageProps, Link, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-const IndexPage = () => (
+type DataProps = {
+  site: {
+    buildTime: string
+  }
+}
+
+const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
   <Layout>
     <Seo title="Home" />
     <h1>Hi people</h1>
@@ -15,15 +21,21 @@ const IndexPage = () => (
       src="../images/gatsby-astronaut.png"
       width={300}
       quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
       alt="A Gatsby astronaut"
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
   </Layout>
 )
 
-export default IndexPage
+export default UsingTypescript
+
+export const query = graphql`
+  {
+    site {
+      buildTime(formatString: "YYYY-MM-DD hh:mm a z")
+    }
+  }
+`
