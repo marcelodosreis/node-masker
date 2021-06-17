0bed5339fc8586f58e9f3b7794eb9adbbd976e30
marcelo.reis1 <marcelo.reis@sympla.com.br>
2021-06-17 15:27:57 -0300

    chore: fix audit and new site

--- public/gatsby-config.js
+++ public/gatsby-config.js
@@ -1,37 +1,15 @@
 module.exports = {
-  pathPrefix: "/node-masker",
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-  },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "smooth-doc",
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: "Node Masker",
+        description:
+          "Node-masker is a javascript mask library made in TypeScript.",
+        siteUrl: "https://example.com",
+        githubRepositoryURL: "https://github.com/marcelodosreis/node-masker",
+        navItems: [{ title: "Docs", url: "/docs/" }],
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-gatsby-cloud`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
-}
+};
